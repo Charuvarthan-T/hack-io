8ae9fa03a38d6d9ee09a1fa0bf8531f3c6336be9
Charuvarthan-T <charuvarthan05@gmail.com>
2026-02-25 00:15:29 +0530

    route.ts after leaderboard integration and push to github

--- app/api/users/[id]/role/route.ts
+++ app/api/users/[id]/role/route.ts
@@ -1,6 +1,5 @@
 import { assignRoleToUser } from "@/repository/user.repository";
 import { NextRequest, NextResponse } from "next/server";
-
 export async function POST(
   req: NextRequest,
   { params }: { params: Promise<{ id: string }> }
@@ -9,21 +8,16 @@ export async function POST(
     const { id } = await params;
     const body = await req.json();
     const { newRole } = body;
-
     await assignRoleToUser(id, newRole);
-
     return NextResponse.json(
       { message: "Role assigned successfully" },
       { status: 200 }
     );
   } catch (e) {
     console.error("Failed to assign role:", e);
-
     return NextResponse.json(
       { error: "Failed to assign role" },
       { status: 500 }
     );
   }
 }
-
-
