6ad1d065f4cd01e4cfb068330a7dc1b73efc5e40
Charuvarthan-T <charuvarthan05@gmail.com>
2026-02-25 00:14:26 +0530

    columns.tsx after leaderboard integration and push to github

--- app/(codeproctor)/(admin)/departments/columns.tsx
+++ app/(codeproctor)/(admin)/departments/columns.tsx
@@ -12,7 +12,6 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { ColumnDef } from "@tanstack/react-table";
 import { toast } from "sonner";
-
 export const createDepartmentColumns = (
   refetchData: () => Promise<void>,
   openEditDialog: (department: department) => void,
@@ -75,11 +74,9 @@ export const createDepartmentColumns = (
       const handleDeleteDepartment = async () => {
         onDeleteDepartment(row.original);
       };
-
       const handleEditDepartment = () => {
         openEditDialog(row.original);
       };
-
       return (
         <>
           <DropdownMenu>
