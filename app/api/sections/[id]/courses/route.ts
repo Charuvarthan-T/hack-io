618c9ce51c6e42d10976756057777c01cd4ed49d
Charuvarthan-T <charuvarthan05@gmail.com>
2026-02-25 00:15:24 +0530

    route.ts after leaderboard integration and push to github

--- app/api/sections/[id]/courses/route.ts
+++ app/api/sections/[id]/courses/route.ts
@@ -1,16 +1,11 @@
 import { getCoursesForSection } from "@/repository/section.repository";
-
 export async function GET(request: Request, { params }: { params: { id: string } }) {
   const { id } = await params;
-  // Fetch courses for the given section ID from your database or service
   const courses = await getCoursesForSection(id);
-
   return new Response(JSON.stringify(courses), {
     headers: { "Content-Type": "application/json" },
   });
 }
-
 export async function POST(request: Request, { params }: { params: { id: string } }) {
   const { id } = await params;
-
 }
