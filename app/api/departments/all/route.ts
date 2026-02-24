358147377b4f19f3a05f6a4b263c32571ee6f5fa
Charuvarthan-T <charuvarthan05@gmail.com>
2026-02-25 00:15:04 +0530

    route.ts after leaderboard integration and push to github

--- app/api/departments/all/route.ts
+++ app/api/departments/all/route.ts
@@ -1,5 +1,4 @@
 import { getAllDepartments } from "@/repository/department.repository";
-
 export async function GET(){
     const departments = await getAllDepartments();
     return new Response(JSON.stringify(departments), {
