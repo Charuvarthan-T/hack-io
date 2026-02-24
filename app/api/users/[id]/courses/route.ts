1208342098287a7c3001e03b2fc69479f6649282
Charuvarthan-T <charuvarthan05@gmail.com>
2026-02-25 00:15:28 +0530

    route.ts after leaderboard integration and push to github

--- app/api/users/[id]/courses/route.ts
+++ app/api/users/[id]/courses/route.ts
@@ -3,19 +3,15 @@ import { getCoursesBySemester } from "@/repository/semester-course.repository";
 import { getCoursesByUserId } from "@/repository/semester.repository";
 import { getMyCoursesForFaculty } from "@/repository/user.repository";
 import { NextRequest } from "next/server";
-
 export async function GET(
   request: NextRequest,
   { params }: { params: { id: string } }
 ) {
   const user = await requireAuth();
-
   if ("status" in user) {
     return user;
   }
-
   const { id: targetUserId } = await params;
-
   if (user.role === "admin") {
     const courses = await getMyCoursesForFaculty(targetUserId);
     return new Response(JSON.stringify({ courses }), {
