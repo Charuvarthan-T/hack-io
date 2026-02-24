df04fa7eecd7d7573cdea5874a79e8c7cf3f5169
Charuvarthan-T <charuvarthan05@gmail.com>
2026-02-25 00:15:29 +0530

    page.tsx after leaderboard integration and push to github

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
 "use client";
-
 import { Header } from "@/components/header";
 import { HeroContent } from "@/components/hero-content";
-
 export default function HomePage() {
   return (
     <div>
