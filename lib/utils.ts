da578188815fdb19ff01c4f3c762db688e1c7518
Charuvarthan-T <charuvarthan05@gmail.com>
2026-02-25 00:15:56 +0530

    utils.ts after leaderboard integration and push to github

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,10 +1,8 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
-
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
-
 export function capitalizeFirstLetter(word: string): string {
   if (!word) return word;
   return word.charAt(0).toUpperCase() + word.slice(1);
