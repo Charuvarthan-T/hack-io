da0bc54af10523538643b1aec112deab4555c033
Charuvarthan-T <charuvarthan05@gmail.com>
2026-02-25 00:15:33 +0530

    header.tsx after leaderboard integration and push to github

--- components/header.tsx
+++ components/header.tsx
@@ -4,10 +4,8 @@ import { signOut, useSession, signIn } from "next-auth/react";
 import { Button } from "./ui/button";
 import Link from "next/link";
 import Image from "next/image";
-
 export function Header() {
   const { data: session } = useSession();
-
   return (
     <header className="border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="px-2 py-4 flex justify-between items-center">
@@ -17,8 +15,7 @@ export function Header() {
             <h1 className="text-2xl font-bold text-foreground">Hack.io</h1>
           </Link>
         </div>
-
-        {/* Conditional User Section */}
+        {}
         {session ? (
           <div className="flex items-center space-x-4">
             <div className="flex items-center space-x-3">
